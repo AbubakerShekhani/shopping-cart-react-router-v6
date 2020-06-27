78939e7cf329d3dda4567f7b5e73638dc6b77745
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-06-27 14:20:34 +0500

    Implemt useNavigate hook of react router dom

--- src/pages/shop/index.jsx
+++ src/pages/shop/index.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import Main from '../../components/Main'
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const Shop = () => {
+
+  let navigate = useNavigate();
+
+  function handleClick() {
+    navigate('/')
+  }
 
   return (
     <Main title="Shop Now" description="Store page" >
       <div>
-        <div className="text-center mt-5">
+        <div className="text-center mt-5 mb-5">
           <h1>Shoe Store</h1>
+          <button onClick={handleClick} className="btn btn-primary btn-sm" >Home</button>
         </div>
         <Outlet />
       </div>
