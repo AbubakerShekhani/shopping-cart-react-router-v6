5f3f8fdb9887e15da30e6302e9a36cf114e8ad95
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-06-27 14:19:57 +0500

    Create home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Main from '../components/Main';
+import { Link } from "react-router-dom";
+import logo from '../Nike-Logo-Practice-App.png'
+
+const Home = () => {
+
+  return (
+    <Main title="Cart" description="Shopping Cart" >
+      <div className="text-center mt-5">
+        <h1>Shoe Store</h1>
+      </div>
+      <div className="text-center mb-20">
+        <Link to="shop">Shop Now</Link>
+      </div>
+      <div className="text-center  mt-5 mb-5">
+        <img src={logo} className="App-logo" alt="logo" width="500" />
+      </div>
+    </Main>
+  )
+}
+
+export default Home
